b3b40eb49b8678552cd148c0b7c59439c1e313ca
samueljrz <samuelevangelista.ti@gmail.com>
2021-03-10 00:18:34 -0300

    initial backend project

---
+++ backend/src/server.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import cors from 'cors';
+import routes from './routes';
+import { errors } from 'celebrate'
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.use(errors());
+
+app.listen(3333);
+
