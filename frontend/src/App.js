64827aa421269fe18a7826f830fd052bba3ebe75
samueljrz <samuelevangelista.ti@gmail.com>
2021-03-24 03:00:47 -0300

    initial page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,9 +8,9 @@ import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
 const theme = createMuiTheme({
 	palette: {
 		primary: {
-			light: '#9c786c',
-			main: '#6d4c41',
-			dark: '#40241a',
+			light: '#F58072',
+			main: '#DB7267',
+			dark: '#B86157',
 			contrastText: '#fff'
 		}
 	}
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -4,14 +4,16 @@ import { Route, BrowserRouter } from 'react-router-dom';
 import SignIn from './components/Login'
 import SignUp from './components/CreateUser'
 import Home from './Home'
+import InitialPage from './components/InitialPage'
 
 
 const Routes = () => {
   return (
     <BrowserRouter>
-      <Route component={Home} path="/home" />
+      <Route exact component={InitialPage} path="/" />
       <Route component={SignUp} path="/signup" />
-      <Route exact component={SignIn} path="/" />
+      <Route component={SignIn} path="/signin" />
+      <Route component={Home} path="/home" /> 
     </BrowserRouter>
   );
 }
