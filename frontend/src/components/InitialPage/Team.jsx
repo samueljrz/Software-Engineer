dd4f1039854edfffc9cc3120d442d0c50b4a2b10
samueljrz <samuelevangelista.ti@gmail.com>
2021-03-25 12:13:58 -0300

    frontend alter

--- frontend/src/components/InitialPage/Team.jsx
+++ frontend/src/components/InitialPage/Team.jsx
@@ -3,27 +3,52 @@ export const Team = (props) => {
     <div id='team' className='text-center'>
       <div className='container'>
         <div className='col-md-8 col-md-offset-2 section-title'>
-          <h2>Meet the Team</h2>
+          <h2>Encontre o time</h2>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
-            dapibus leonec.
+            Alunos da Universidade Federal do Ceará
           </p>
         </div>
         <div id='row'>
-          {props.data
-            ? props.data.map((d, i) => (
-                <div key={`${d.name}-${i}`} className='col-md-3 col-sm-6 team'>
-                  <div className='thumbnail'>
-                    {' '}
-                    <img src={d.img} alt='...' className='team-img' />
-                    <div className='caption'>
-                      <h4>{d.name}</h4>
-                      <p>{d.job}</p>
-                    </div>
-                  </div>
-                </div>
-              ))
-            : 'loading'}
+          <div key="0" className='col-md-3 col-sm-6 team'>
+            <div className='thumbnail'>
+              {' '}
+              <img src="https://avatars.githubusercontent.com/u/37130241?v=4" alt='...' className='team-img' />
+              <div className='caption'>
+                <h4>Samuel Evangelista</h4>
+                <p>Full Stack Developer & <br/> Product Owner</p>
+              </div>
+            </div>
+          </div>
+          <div key="1" className='col-md-3 col-sm-6 team'>
+            <div className='thumbnail'>
+              {' '}
+              <img src="https://avatars.githubusercontent.com/u/37377587?v=4" alt='...' className='team-img' />
+              <div className='caption'>
+                <h4>Guilherme Willian</h4>
+                <p>Full Stack Developer & <br/> Designer </p>
+              </div>
+            </div>
+          </div>
+          <div key="2" className='col-md-3 col-sm-6 team'>
+            <div className='thumbnail'>
+              {' '}
+              <img src="https://avatars.githubusercontent.com/u/47836518?v=4" alt='...' className='team-img' />
+              <div className='caption'>
+                <h4>Francisco Valdemir</h4>
+                <p>Designer</p>
+              </div>
+            </div>
+          </div>
+          <div key="3" className='col-md-3 col-sm-6 team'>
+            <div className='thumbnail'>
+              {' '}
+              <img src="https://avatars.githubusercontent.com/u/55724422?v=4" alt='...' className='team-img' />
+              <div className='caption'>
+                <h4>Francisco Jerônimo</h4>
+                <p>Tester</p>
+              </div>
+            </div>
+          </div> 
         </div>
       </div>
     </div>
--- frontend/src/components/InitialPage/header.jsx
+++ frontend/src/components/InitialPage/header.jsx
@@ -7,15 +7,15 @@ export const Header = (props) => {
             <div className='row'>
               <div className='col-md-8 col-md-offset-2 intro-text'>
                 <h1>
-                  {props.data ? props.data.title : 'Loading'}
+                  Sua aprovação está aqui
                   <span></span>
                 </h1>
-                <p>{props.data ? props.data.paragraph : 'Loading'}</p>
+                <p>Somos a melhor e a mais moderna plataforma de questões no mercado, com vários alunos aprovados, venha conquistar sua aprovação também.</p>
                 <a
                   href='#features'
                   className='btn btn-custom btn-lg page-scroll'
                 >
-                  Learn More
+                  Saiba Mais
                 </a>{' '}
               </div>
             </div>
