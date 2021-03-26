baeca923291afcfe548f1be559b16c7fcbfd70c6
samueljrz <samuelevangelista.ti@gmail.com>
2021-03-26 01:34:04 -0300

    add questions component unfinished

---
+++ frontend/src/components/Questions/index.jsx
@@ -0,0 +1,121 @@
+import React from 'react'
+import { Component } from 'react';
+import withStyles from '@material-ui/core/styles/withStyles';
+//import axios from 'axios';
+
+import './styles.css'
+
+class Question extends Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return (
+      <div className="main">
+        <div className="container">
+          <div className="container-question"> 
+            <div className="container-content">
+              <div className="questionid">questao id </div>
+              <div> disciplina </div>
+            </div>
+            <div className="container-info">
+              <span>Ano: 2020</span>
+              <span>Banca: CESPE</span>
+              <span>Nivel: Superior</span>
+              <span>Instituição: Policia Federal</span>
+            </div>
+            <div className="line"></div>
+            <div className="container-proof">
+              <div>“Trata-se de um crédito adicional destinado a suprir despesas urgentes e imprevistas, não dependem de fonte de recursos e independem de autorização legislativa, devendo ser abertos por Medida Provisória (União) ou Decreto do Poder Executivo (Estados e Municípios) que devem dar imediata ciência ao Poder Legislativo.” As informações correspondem a crédito:</div>
+              <div>assertivas</div>
+              <div>button</div>
+            </div>
+          </div>
+        </div>
+        <div className="container">
+          <div className="container-question"> 
+            <div className="container-content">
+              <div className="questionid">questao id </div>
+              <div> disciplina </div>
+            </div>
+            <div className="container-info">
+              <span>Ano: 2020</span>
+              <span>Banca: CESPE</span>
+              <span>Nivel: Superior</span>
+              <span>Instituição: Policia Federal</span>
+            </div>
+            <div className="line"></div>
+            <div className="container-proof">
+              <div>“Trata-se de um crédito adicional destinado a suprir despesas urgentes e imprevistas, não dependem de fonte de recursos e independem de autorização legislativa, devendo ser abertos por Medida Provisória (União) ou Decreto do Poder Executivo (Estados e Municípios) que devem dar imediata ciência ao Poder Legislativo.” As informações correspondem a crédito:</div>
+              <div>assertivas</div>
+              <div>button</div>
+            </div>
+          </div>
+        </div>
+        <div className="container">
+          <div className="container-question"> 
+            <div className="container-content">
+              <div className="questionid">questao id </div>
+              <div> disciplina </div>
+            </div>
+            <div className="container-info">
+              <span>Ano: 2020</span>
+              <span>Banca: CESPE</span>
+              <span>Nivel: Superior</span>
+              <span>Instituição: Policia Federal</span>
+            </div>
+            <div className="line"></div>
+            <div className="container-proof">
+              <div>“Trata-se de um crédito adicional destinado a suprir despesas urgentes e imprevistas, não dependem de fonte de recursos e independem de autorização legislativa, devendo ser abertos por Medida Provisória (União) ou Decreto do Poder Executivo (Estados e Municípios) que devem dar imediata ciência ao Poder Legislativo.” As informações correspondem a crédito:</div>
+              <div>assertivas</div>
+              <div>button</div>
+            </div>
+          </div>
+        </div>
+        <div className="container">
+          <div className="container-question"> 
+            <div className="container-content">
+              <div className="questionid">questao id </div>
+              <div> disciplina </div>
+            </div>
+            <div className="container-info">
+              <span>Ano: 2020</span>
+              <span>Banca: CESPE</span>
+              <span>Nivel: Superior</span>
+              <span>Instituição: Policia Federal</span>
+            </div>
+            <div className="line"></div>
+            <div className="container-proof">
+              <div>“Trata-se de um crédito adicional destinado a suprir despesas urgentes e imprevistas, não dependem de fonte de recursos e independem de autorização legislativa, devendo ser abertos por Medida Provisória (União) ou Decreto do Poder Executivo (Estados e Municípios) que devem dar imediata ciência ao Poder Legislativo.” As informações correspondem a crédito:</div>
+              <div>assertivas</div>
+              <div>button</div>
+            </div>
+          </div>
+        </div>
+        <div className="container" style={{marginBottom: "40px"}}>
+          <div className="container-question"> 
+            <div className="container-content">
+              <div className="questionid">questao id </div>
+              <div> disciplina </div>
+            </div>
+            <div className="container-info">
+              <span>Ano: 2020</span>
+              <span>Banca: CESPE</span>
+              <span>Nivel: Superior</span>
+              <span>Instituição: Policia Federal</span>
+            </div>
+            <div className="line"></div>
+            <div className="container-proof">
+              <div>“Trata-se de um crédito adicional destinado a suprir despesas urgentes e imprevistas, não dependem de fonte de recursos e independem de autorização legislativa, devendo ser abertos por Medida Provisória (União) ou Decreto do Poder Executivo (Estados e Municípios) que devem dar imediata ciência ao Poder Legislativo.” As informações correspondem a crédito:</div>
+              <div>assertivas</div>
+              <div>button</div>
+            </div>
+          </div>
+        </div>
+      </div>
+		);
+	}
+}
+
+export default Question;
